ec3deed9de1ba02586f027402f27f9c397e95ae6
0老船长0 <41262252+0oldcaptain0@users.noreply.github.com>
2018-07-25 14:19:08 +0800

    Add files via upload

---
+++ xunke201807021/src/com/Test.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet {
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		req.setCharacterEncoding("utf-8");
+		String username=req.getParameter("name");
+		String pwd=req.getParameter("pwd");
+		
+		System.out.printf("”√ªß√˚:"+username+"\n"+"√‹¬Î:"+pwd+"\n");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+}
